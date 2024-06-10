4934173a3571d1914ea3d65417a675438e7f682c
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-06-10 17:28:13 +0200

    [UPD] Font-awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import { createApp } from "vue";
 import "./styles/style.scss";
 import App from "./App.vue";
 
+// ROUTER
 import { router } from "./router.js";
 
-createApp(App).use(router).mount("#app");
+// FONT AWESOME
+import { library } from "@fortawesome/fontawesome-svg-core";
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+/* import specific icons */
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret, faSearch);
+
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
+  .mount("#app");
--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -1,7 +1,9 @@
 <script></script>
 
 <template>
-    <h1>About</h1>
+  <h1>About</h1>
+  <font-awesome-icon icon="fa-solid fa-user-secret" />
+  <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
 </template>
 
 <style scoped></style>
