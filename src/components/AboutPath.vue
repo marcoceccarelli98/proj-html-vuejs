fa766687c936963f7872a7b7a7e55277bcb77df5
Rachele-Turtora <racheleturtora21@gmail.com>
2024-06-10 22:21:58 +0200

    [UPD] AboutPath component

---
+++ src/components/AboutPath.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "AboutPath"
+}
+</script>
+
+<template>
+    <div class="container">
+        <h2>About</h2>
+        <p>Home / About</p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/style.scss" as *;
+
+.container {
+    background-color: $primary-color;
+    color: white;
+}
+
+h2 {
+    font-size: 40px;
+    padding: 50px 0 0 40px;
+}
+
+p {
+    padding: 20px 0 50px 40px;
+}
+</style>
--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -1,9 +1,19 @@
-<script></script>
+<script>
+import AboutPath from "../components/AboutPath.vue"
+
+export default {
+  name: "AppAbout",
+  components: {
+    AboutPath
+  }
+}
+</script>
 
 <template>
   <h1>About</h1>
   <font-awesome-icon icon="fa-solid fa-user-secret" />
   <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
+  <AboutPath />
 </template>
 
 <style scoped></style>
