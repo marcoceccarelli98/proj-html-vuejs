cb351222f20b57e5ca63018f083b15b165be69ba
Rachele-Turtora <racheleturtora21@gmail.com>
2024-06-11 09:18:04 +0200

    [FIX] PagePath (effetto parallasse)

---
+++ src/components/PagePath.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    name: "PagePath"
+}
+</script>
+
+<template>
+    <div class="parallax-container">
+        <div class="container">
+            <h2>About</h2>
+            <p>Home / About</p>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/style.scss" as *;
+
+.parallax-container {
+    position: relative;
+    height: 250px;
+    display: flex;
+    align-items: center;
+    overflow: hidden;
+    background-image: url('/pages-title-parallax.jpg');
+    background-size: cover;
+    background-attachment: fixed;
+    /* Effetto parallasse */
+    background-position: center;
+    color: white;
+}
+
+.container {
+    width: 90%;
+    height: 100px;
+    margin: 0 auto;
+}
+
+h2 {
+    font-size: 40px;
+    margin-bottom: 20px;
+}
+</style>
--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -1,11 +1,11 @@
 <script>
-import AboutPath from "../components/AboutPath.vue"
+import PagePath from "../components/PagePath.vue"
 import EventsComponent from "../components/EventsComponent.vue";
 
 export default {
   name: "AppAbout",
   components: {
-    AboutPath,
+    PagePath,
     EventsComponent
   }
 }
@@ -15,11 +15,8 @@ export default {
   <h1>About</h1>
   <font-awesome-icon icon="fa-solid fa-user-secret" />
   <font-awesome-icon :icon="['fas', 'magnifying-glass']" />
-  <div class="bg-primary">
-    <div class="container">
-      <AboutPath />
-    </div>
-  </div>
+
+  <PagePath />
   <div class="container">
     <EventsComponent />
   </div>
@@ -27,10 +24,6 @@ export default {
 
 <style scoped lang="scss">
 @use "../styles/style.scss" as *;
-
-.bg-primary {
-  background-color: $primary-color;
-}
 
 .container {
   width: 90%;
